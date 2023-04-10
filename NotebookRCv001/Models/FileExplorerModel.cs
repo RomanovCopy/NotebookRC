9f852fae1bba3b091a330543caa94c204ff9f530
Сергей Романов <RomanovCopy@gmail.com>
2023-04-10 14:06:52 +0300

    fix

---
+++ NotebookRCv001/Models/FileExplorerModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using NotebookRCv001.Infrastructure;
+using NotebookRCv001.Interfaces;
+
+namespace NotebookRCv001.Models
+{
+    internal class FileExplorerModel:ViewModelBase
+    {
+
+
+
+        public ObservableCollection<string> Headers => throw new NotImplementedException();
+
+        public ObservableCollection<string> ToolTips => throw new NotImplementedException();
+
+        public Action<object> BehaviorReady { get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); }
+
+
+
+        internal FileExplorerModel()
+        {
+
+        }
+
+        internal bool CanExecute_PageLoaded(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Execute_PageLoaded(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool CanExecute_PageClose(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Execute_PageClose(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool CanExecute_PageClear(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Execute_PageClear(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NotebookRCv001/ViewModels/FileExplorerViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NotebookRCv001.Interfaces;
+using NotebookRCv001.Infrastructure;
+using NotebookRCv001.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace NotebookRCv001.ViewModels
+{
+    public class FileExplorerViewModel:ViewModelBase, IPageViewModel
+    {
+        private readonly FileExplorerModel fileExplorerModel;
+
+        public ObservableCollection<string> Headers => fileExplorerModel.Headers;
+        public ObservableCollection<string> ToolTips => fileExplorerModel.ToolTips;
+        public Action<object> BehaviorReady { get => fileExplorerModel.BehaviorReady;
+            set => fileExplorerModel.BehaviorReady = value; }
+
+
+
+        public FileExplorerViewModel()
+        {
+            fileExplorerModel = new();
+            fileExplorerModel.PropertyChanged += (s, e) => OnPropertyChanged(e.PropertyName);
+
+        }
+
+
+        public ICommand PageLoaded => pageLoaded ??= new RelayCommand(fileExplorerModel.Execute_PageLoaded,
+            fileExplorerModel.CanExecute_PageLoaded);
+        private RelayCommand pageLoaded;
+
+        public ICommand PageClose => pageClose ??= new RelayCommand(fileExplorerModel.Execute_PageClose,
+            fileExplorerModel.CanExecute_PageClose);
+        private RelayCommand pageClose;
+
+        public ICommand PageClear => pageClear ??= new RelayCommand(fileExplorerModel.Execute_PageClear,
+            fileExplorerModel.CanExecute_PageClear);
+        private RelayCommand pageClear;
+    }
+}
