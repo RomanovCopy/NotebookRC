7c884fff1f080d50ca07a19d386bc57f64783fef
Сергей Романов <RomanovCopy@gmail.com>
2023-04-12 10:58:08 +0300

    fix

--- NotebookRCv001/Models/FileExplorerModel.cs
+++ NotebookRCv001/Models/FileExplorerModel.cs
@@ -30,10 +30,44 @@ namespace NotebookRCv001.Models
         internal ObservableCollection<DriveInfo> DriveInfos { get => driveInfos; set => SetProperty(ref driveInfos, value); }
         private ObservableCollection<DriveInfo> driveInfos;
         /// <summary>
+        /// коллекция элементов из отображаемой директории
+        /// </summary>
+        internal ObservableCollection<DirectoryItem> CurrentDirectoryList { get => currentDirectoryList;
+            set => SetProperty(ref currentDirectoryList, value); }
+        private ObservableCollection<DirectoryItem> currentDirectoryList;
+        /// <summary>
+        /// коллекция размеров обложек
+        /// </summary>
+        internal ObservableCollection<int> CoverSizes { get => coverSizes; set => SetProperty(ref coverSizes, value); }
+        private ObservableCollection<int> coverSizes;
+        /// <summary>
         /// индекс выбранного элемента коллекции доступных дисков
         /// </summary>
         internal int SelectedIndexDrives { get => selectedIndexDrives; set => SetProperty(ref selectedIndexDrives, value); }
         private int selectedIndexDrives;
+        /// <summary>
+        /// индекс выбранного размера обложки в коллекции CoverSizes
+        /// </summary>
+        internal int CoverSizesIndex { get => coverSizesIndex; set => SetProperty(ref coverSizesIndex, value); }
+        private int coverSizesIndex;
+        /// <summary>
+        /// полный путь к текущей дирректории
+        /// </summary>
+        internal string CurrentDirectoryFullName { get => currentDirectoryFullName; 
+            set => SetProperty(ref currentDirectoryFullName, value); }
+        private string currentDirectoryFullName;
+        /// <summary>
+        /// флаг: отображение обложек
+        /// </summary>
+        internal bool IsCoverEnabled { get => isCoverEnabled; set => SetProperty(ref isCoverEnabled, value); }
+        private bool isCoverEnabled;
+        /// <summary>
+        /// флаг: отображение в виде плиток
+        /// </summary>
+        internal bool IsTilesEnabled { get => isTilesEnabled; set => SetProperty(ref isTilesEnabled, value); }
+        private bool isTilesEnabled;
+
+
 
         internal FileExplorerModel()
         {
@@ -43,6 +77,34 @@ namespace NotebookRCv001.Models
 
 
 
+        /// <summary>
+        /// активация/дезактивация CheckedIsTilesEnabled
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal bool CanExecute_CheckedIsTilesEnabled(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Execute_CheckedIsTilesEnabled(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// активация/дезактивация IsCoverEnabled
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal bool CanExecute_CheckedIsCover(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Execute_CheckedIsCover(object obj)
+        {
+            throw new NotImplementedException();
+        }
         /// <summary>
         /// переход к родительскому элементу
         /// </summary>
@@ -54,6 +116,20 @@ namespace NotebookRCv001.Models
             throw new NotImplementedException();
         }
         internal void Execute_ClickToParentDirectory(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// изменение выбора размера обложки
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal bool CanExecute_CoverSizesSelectionChanged(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Execute_CoverSizesSelectionChanged(object obj)
         {
             throw new NotImplementedException();
         }
@@ -71,7 +147,20 @@ namespace NotebookRCv001.Models
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// окончание загрузки коллекции размеров обложек (ComboBox)
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal bool CanExecute_CoverSizesLoaded(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Execute_CoverSizesLoaded(object obj)
+        {
+            throw new NotImplementedException();
+        }
         /// <summary>
         /// окончание загрузки коллекции доступных дисков(ComboBox)
         /// </summary>
--- NotebookRCv001/ViewModels/FileExplorerViewModel.cs
+++ NotebookRCv001/ViewModels/FileExplorerViewModel.cs
@@ -21,9 +21,21 @@ namespace NotebookRCv001.ViewModels
         public ObservableCollection<string> ToolTips => fileExplorerModel.ToolTips;
         public Action<object> BehaviorReady { get => fileExplorerModel.BehaviorReady;
             set => fileExplorerModel.BehaviorReady = value; }
+        public ObservableCollection<DriveInfo> DriveInfos { get => fileExplorerModel.DriveInfos; 
+            set => fileExplorerModel.DriveInfos=value; }
+        public ObservableCollection<DirectoryItem> CurrentDirectoryList
+        { get => fileExplorerModel.CurrentDirectoryList; set => fileExplorerModel.CurrentDirectoryList = value; }
+        public ObservableCollection<int> CoverSizes { get => fileExplorerModel.CoverSizes;
+            set => fileExplorerModel.CoverSizes = value; }
+        public int SelectedIndexDrives { get => fileExplorerModel.SelectedIndexDrives; 
+            set => fileExplorerModel.SelectedIndexDrives = value; }
+        public int CoverSizesIndex { get => fileExplorerModel.CoverSizesIndex;
+            set => fileExplorerModel.CoverSizesIndex = value; }
+        public string CurrentDirectoryFullName { get => fileExplorerModel.CurrentDirectoryFullName; 
+            set => fileExplorerModel.CurrentDirectoryFullName = value; }
+        public bool IsCoverEnabled { get => fileExplorerModel.IsCoverEnabled; set => fileExplorerModel.IsCoverEnabled = value; }
+        public bool IsTilesEnabled { get => fileExplorerModel.IsTilesEnabled; set => fileExplorerModel.IsTilesEnabled = value; }
 
-        public ObservableCollection<DriveInfo> DriveInfos { get => fileExplorerModel.DriveInfos; set => fileExplorerModel.DriveInfos=value; }
-        public int SelectedIndexDrives { get => fileExplorerModel.SelectedIndexDrives; set => fileExplorerModel.SelectedIndexDrives = value; }
 
         public FileExplorerViewModel()
         {
@@ -32,24 +44,34 @@ namespace NotebookRCv001.ViewModels
 
         }
 
+
+        public ICommand CheckedIsTilesEnabled => checkedIsTilesEnabled ??= 
+            new RelayCommand(fileExplorerModel.Execute_CheckedIsTilesEnabled, fileExplorerModel.CanExecute_CheckedIsTilesEnabled);
+        private RelayCommand checkedIsTilesEnabled;
+        public ICommand CheckedIsCover => checkedIsCover ??= new RelayCommand(fileExplorerModel.Execute_CheckedIsCover,
+            fileExplorerModel.CanExecute_CheckedIsCover);
+        private RelayCommand checkedIsCover;
         public ICommand ClickToParentDirectory => clickToParentDirectory ??= new RelayCommand(
             fileExplorerModel.Execute_ClickToParentDirectory, fileExplorerModel.CanExecute_ClickToParentDirectory);
         private RelayCommand clickToParentDirectory;
         public ICommand ComboBoxDrivesSelectionChanged => comboBoxDrivesSelectionChanged ??= new RelayCommand(
             fileExplorerModel.Execute_ComboBoxDrivesSelectionChanged, fileExplorerModel.CanExecute_ComboBoxDrivesSelectionChanged);
         private RelayCommand comboBoxDrivesSelectionChanged;
+        public ICommand CoverSizesSelectionChanged => coverSizesSelectionChanged ??= new RelayCommand(
+            fileExplorerModel.Execute_CoverSizesSelectionChanged, fileExplorerModel.CanExecute_CoverSizesSelectionChanged);
+        private RelayCommand coverSizesSelectionChanged;
+        public ICommand CoverSizesLoaded => coverSizesLoaded ??= new RelayCommand(fileExplorerModel.Execute_CoverSizesLoaded,
+            fileExplorerModel.CanExecute_CoverSizesLoaded);
+        private RelayCommand coverSizesLoaded;
         public ICommand ComboBoxDrivesLoaded => comboBoxDrivesLoaded ??= new RelayCommand(fileExplorerModel.Execute_ComboBoxDrivesLoaded,
             fileExplorerModel.CanExecute_ComboBoxDrivesLoaded);
         private RelayCommand comboBoxDrivesLoaded;
-
         public ICommand PageLoaded => pageLoaded ??= new RelayCommand(fileExplorerModel.Execute_PageLoaded,
             fileExplorerModel.CanExecute_PageLoaded);
         private RelayCommand pageLoaded;
-
         public ICommand PageClose => pageClose ??= new RelayCommand(fileExplorerModel.Execute_PageClose,
             fileExplorerModel.CanExecute_PageClose);
         private RelayCommand pageClose;
-
         public ICommand PageClear => pageClear ??= new RelayCommand(fileExplorerModel.Execute_PageClear,
             fileExplorerModel.CanExecute_PageClear);
         private RelayCommand pageClear;
