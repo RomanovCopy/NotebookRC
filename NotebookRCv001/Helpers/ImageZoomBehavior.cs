f61124dda42973530b1e53679ec04f57d12c92a2
RomanovCopy <RomanovCopy@gmail.com>
2023-06-13 20:36:36 +0300

    fix

--- NotebookRCv001/Helpers/ImageZoomBehavior.cs
+++ NotebookRCv001/Helpers/ImageZoomBehavior.cs
@@ -15,13 +15,14 @@ namespace NotebookRCv001.Helpers
     {
         private Point _lastMousePosition;
         private bool _isMouseDragging;
+        private double scale = 1.0; // Глобальная переменная для масштаба
 
         protected override void OnAttached()
         {
             base.OnAttached();
             AssociatedObject.MouseWheel += Image_MouseWheel;
             AssociatedObject.MouseLeftButtonDown += Image_MouseLeftButtonDown;
-            AssociatedObject.MouseMove += Image_MouseMove;
+            //AssociatedObject.MouseMove += Image_MouseMove;
             AssociatedObject.MouseLeftButtonUp += Image_MouseLeftButtonUp;
         }
 
@@ -30,31 +31,47 @@ namespace NotebookRCv001.Helpers
             base.OnDetaching();
             AssociatedObject.MouseWheel -= Image_MouseWheel;
             AssociatedObject.MouseLeftButtonDown -= Image_MouseLeftButtonDown;
-            AssociatedObject.MouseMove -= Image_MouseMove;
+            //AssociatedObject.MouseMove -= Image_MouseMove;
             AssociatedObject.MouseLeftButtonUp -= Image_MouseLeftButtonUp;
         }
 
         private void Image_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            var transform = AssociatedObject.RenderTransform as MatrixTransform;
-            var matrix = transform.Matrix;
+            var image = sender as Image;
+            var transform = image.RenderTransform;
 
-            var scale = e.Delta > 0 ? 1.1 : 0.9; // Изменение масштаба (увеличение или уменьшение)
-            var position = e.GetPosition(AssociatedObject);
+            ScaleTransform scaleTransform = transform as ScaleTransform;
 
-            var newMatrix = Matrix.Identity;
-            newMatrix.ScaleAt(scale, scale, position.X, position.Y);
-            newMatrix.OffsetX = matrix.OffsetX;
-            newMatrix.OffsetY = matrix.OffsetY;
+            if (scaleTransform == null)
+            {
+                // Если RenderTransform не является экземпляром ScaleTransform, создаем новый экземпляр
+                scaleTransform = new ScaleTransform();
+                image.RenderTransform = scaleTransform;
+            }
 
-            AssociatedObject.RenderTransform = new MatrixTransform(newMatrix);
+            var zoomIncrement = 0.2;
+            var zoomDirection = e.Delta > 0 ? 1 : -1;
+
+            var newScale = scaleTransform.ScaleX + zoomIncrement * zoomDirection;
+
+            // Ограничиваем масштабирование в допустимых пределах
+            var minScale = 0.2;
+            var maxScale = 5.0;
+            if (newScale < minScale)
+                newScale = minScale;
+            else if (newScale > maxScale)
+                newScale = maxScale;
+
+            // Применяем новый масштаб
+            scaleTransform.ScaleX = newScale;
+            scaleTransform.ScaleY = newScale;
+
+            // Центрируем изображение в пределах контейнера
+            CenterImage(image);
         }
 
         private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            var position = e.GetPosition(AssociatedObject);
-            _lastMousePosition = position;
-            _isMouseDragging = true;
             AssociatedObject.CaptureMouse();
         }
 
@@ -78,9 +95,40 @@ namespace NotebookRCv001.Helpers
             }
         }
 
+        private void CenterImage()
+        {
+            var transform = AssociatedObject.RenderTransform as MatrixTransform;
+            var matrix = transform.Matrix;
+
+            var centerOfViewport = new Point(AssociatedObject.ActualWidth / 2, AssociatedObject.ActualHeight / 2);
+            var centerOfImage = transform.Transform(centerOfViewport);
+
+            var offsetX = centerOfViewport.X - centerOfImage.X;
+            var offsetY = centerOfViewport.Y - centerOfImage.Y;
+
+            matrix.Translate(offsetX, offsetY);
+
+            transform.Matrix = matrix;
+        }
+
+        private void CenterImage(Image image)
+        {
+            var container = VisualTreeHelper.GetParent(image) as FrameworkElement;
+            var scaleTransform = image.RenderTransform as ScaleTransform;
+
+            var imageWidth = image.ActualWidth * scaleTransform.ScaleX;
+            var imageHeight = image.ActualHeight * scaleTransform.ScaleY;
+
+            var offsetX = (container.ActualWidth - imageWidth) / 2;
+            var offsetY = (container.ActualHeight - imageHeight) / 2;
+            if (offsetX < 0 || offsetY < 0)
+                return;
+            image.RenderTransformOrigin = new Point(0.5, 0.5);
+            image.RenderTransform = new ScaleTransform(scaleTransform.ScaleX, scaleTransform.ScaleY, offsetX, offsetY);
+        }
+
         private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            _isMouseDragging = false;
             AssociatedObject.ReleaseMouseCapture();
         }
 
