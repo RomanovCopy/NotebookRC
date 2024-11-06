fa895f794dc7fc49420bb55ac2c042f668d2b03a
Сергей Романов <RomanovCopy@gmail.com>
2024-11-06 14:25:54 +0300

    fix0611241425

--- NotebookRCv001/Models/HomeSideMenu/SideMenuModel.cs
+++ NotebookRCv001/Models/HomeSideMenu/SideMenuModel.cs
@@ -8,7 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
 
 
 namespace NotebookRCv001.Models.HomeSideMenu
@@ -19,6 +21,10 @@ namespace NotebookRCv001.Models.HomeSideMenu
         private readonly MainWindowViewModel mainWindowViewModel;
 
         internal ObservableCollection<MenuItem> MenuItems { get; set; }
+
+
+        internal Dictionary<string, BitmapSource> Icons { get=>icons; private set=>icons=value; }
+        Dictionary<string, BitmapSource> icons;
 
 
         internal Languages Language { set => SetProperty(ref language, value); get => language; }
@@ -43,6 +49,7 @@ namespace NotebookRCv001.Models.HomeSideMenu
             language = mainWindowViewModel.Language;
             language.PropertyChanged += (s, e) => OnPropertyChanged(new string[] { "Headers", "ToolTips" });
             MenuItems = Create_MenuItems();
+            CreateIcons();
         }
 
 
@@ -51,11 +58,11 @@ namespace NotebookRCv001.Models.HomeSideMenu
 
         internal bool CanExecute_PageLoaded(object obj)
         {
-            throw new NotImplementedException();
+            return true;
         }
         internal void Execute_PageLoaded(object obj)
         {
-            throw new NotImplementedException();
+            var a = "Ok";
         }
 
 
@@ -89,5 +96,16 @@ namespace NotebookRCv001.Models.HomeSideMenu
             return menu;
         }
 
+        private void CreateIcons()
+        {
+            // Установка иконки окна на одну из системных иконок
+            var icon = Imaging.CreateBitmapSourceFromHIcon( SystemIcons.Information.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions() );
+            System.Drawing.Icon warningIcon = SystemIcons.Warning; BitmapSource bitmapSource = 
+                Imaging.CreateBitmapSourceFromHIcon( warningIcon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions() );
+            var source = bitmapSource; 
+        }
+
     }
+
+   
 }
